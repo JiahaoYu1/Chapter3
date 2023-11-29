354e4f2457cafc6257d33bf6e74d7cd7aabf4fc1
Jiahao Yu <98350594+JiahaoYu1@users.noreply.github.com>
2023-11-29 14:46:02 -0500

    Add files via upload

---
+++ ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Controllers
+{
+    /*
+* Course: 		Web Programming 3
+* Assessment: 	Milestone 3
+* Created by: 	Jiahao Yu -2134609
+* Date: 		09 Nov 2023
+* Class Name: 	ProductsController.cs
+* Description: 	
+The ProductsController class is an ASP.NET Core controller responsible for 
+    handling HTTP requests related to products, utilizing the functionality provided 
+    by the IProductsProvider interface, and offering endpoints for retrieving all products
+    and a specific product by ID.
+  */
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsProvider productsProvider;
+        public ProductsController(IProductsProvider productsProvider)
+        {
+            this.productsProvider = productsProvider;
+        }
+
+        public IProductsProvider ProductsProvider { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            var result = await productsProvider.GetProductsAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Products);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductAsync(int id)
+        {
+            var result = await productsProvider.GetProductAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Product);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ ECommerce/Interfaces/IProductsProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ECommerce.Api.Products.Db;
+
+namespace ECommerce.Api.Products.Interfaces
+{
+    public interface IProductsProvider
+    {
+        Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync();
+        Task<(bool IsSuccess, Product Product, string ErrorMessage)> GetProductAsync(int id);
+    }
+}
